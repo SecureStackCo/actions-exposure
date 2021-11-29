e89a72da8971d436f2eb01e2b3346e2b6ece91f7
Ben Sullivan <ben@coreillumination.com>
2021-11-29 21:43:20 +1000

    Add entrypoint.sh with execute permissions

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# `$*` expands the `args` supplied in an `array` individually
+# or splits `args` in a string separated by whitespace.
+sh -c "echo $*"
+sh -c "pwd; ls -la;/bin/bloodhound-cli $*"
